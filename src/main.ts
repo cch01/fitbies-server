b3536f2384ef5f7b525b0df8774d3097d6609b2d
cch01 <chanchiho01@gmail.com>
2021-02-21 15:16:51 +0800

    peer server split up

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,8 @@ require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import initPeerServer from './peer.server';
 
 const PORT = process.env.APP_PORT;
-const peerPort = parseInt(process.env.PEER_PORT);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,8 +12,6 @@ async function bootstrap() {
     origin: 'http://localhost:4000',
     credentials: true,
   });
-  initPeerServer(peerPort);
-  console.log(`Peer server listing on ${peerPort}/meetings`);
   app.listen(PORT);
 }
 bootstrap();
